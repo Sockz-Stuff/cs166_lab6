ae110e24a008015598e994b3aa51c647cac29fe2
Marshall Jones <mjone032@wch135-01.cs.ucr.edu>
2022-11-06 14:39:40 -0800

    finished part 1

--- queries.sql
+++ queries.sql
@@ -1,3 +1,6 @@
+drop table suppliersg;
+drop table suppliersr;
+
 /* Find the total number of parts supplied by each supplier */
 SELECT Suppliers.sname, COUNT(Catalog.pid)
 FROM Catalog, Suppliers, Parts
@@ -13,8 +16,33 @@ where catalog.sid = suppliers.sid and catalog.pid = parts.pid
 group by Suppliers.sname
 having COUNT(catalog.pid) >= 3;
 
-SELECT Suppliers.sname, COUNT(Catalog.pid)
+/*For every supplier that supplies only green parts, print the name 
+of the supplier and the total number of parts that he supplies. */
+select suppliers.sname, count(catalog.pid)
 from catalog, suppliers, parts
-where catalog.sid = suppliers.sid and catalog.pid = parts.pid 
-group by Suppliers.sname
-having parts.color = 'green';
+where catalog.sid = suppliers.sid and catalog.pid = parts.pid
+and
+not exists(
+select catalog.sid
+from catalog, suppliers, parts
+where catalog.sid = suppliers.sid and catalog.pid = parts.pid and parts.color != 'Green')
+group by suppliers.sname;
+
+/* For every supplier that supplies green part and red part, print the 
+name of the supplier and the price of the most expensive part that 
+he supplies. pid (1 or 3 or 8) and 9 which means big red tools and perfunctory parts should return */
+
+select catalog.sid, catalog.cost
+into suppliersg
+from catalog, suppliers, parts
+where catalog.sid = suppliers.sid and catalog.pid = parts.pid and parts.color = 'Green';
+
+select catalog.sid, catalog.cost
+into suppliersr
+from catalog, suppliers, parts
+where catalog.sid = suppliers.sid and catalog.pid = parts.pid and parts.color = 'Red';
+
+select suppliers.sname, max(suppliersr.cost)
+from suppliers, suppliersr, suppliersg, catalog
+where suppliersr.sid = suppliersg.sid and suppliersr.sid = suppliers.sid
+group by suppliers.sname;
