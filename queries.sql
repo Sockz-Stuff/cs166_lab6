ee341dd1e7d987dc46354ac59b7cdcdf14ffdff2
Marshall Jones <mjone032@wch135-01.cs.ucr.edu>
2022-11-06 13:05:09 -0800

    woohoo

--- queries.sql
+++ queries.sql
@@ -1,6 +1,15 @@
 /* Find the total number of parts supplied by each supplier */
+SELECT Suppliers.sname, COUNT(Catalog.pid)
+FROM Catalog, Suppliers, Parts
+WHERE Catalog.sid = Suppliers.sid AND Catalog.pid = Parts.pid
+GROUP BY Suppliers.sname;
 
-SELECT COUNT(Parts.pid)
-FROM Parts, Suppliers, Catalog
-WHERE Parts.pid = Catalog.pid AND Suppliers.sid = Catalog.sid;
+
+/*Find  the  total  number  of  parts  supplied  by  each  supplier  who 
+supplies at least 3 parts. */
+SELECT Suppliers.sname, COUNT(Catalog.pid)
+from catalog, suppliers, parts
+where catalog.sid = suppliers.sid and catalog.pid = parts.pid 
+group by Suppliers.sname
+having COUNT(catalog.pid) >= 3;
 
